57b9a882f60efff65274f4bf8f8ea873dfa3f0f8
Dominic Frecentese <domfrecent@gmail.com>
2021-01-04 14:45:29 -0700

    Add timeseries visualization and YoY comparisons

--- analysis.py
+++ analysis.py
@@ -1,14 +1,45 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from datetime import datetime, timedelta
 
-daily_crashes = pd.read_csv("nyc_crash_data/daily_crash_data.csv")
+def get_crash_data():
+	daily_crashes = pd.read_csv("nyc_crash_data/daily_crash_data.csv")
+	daily_crashes.crash_date = pd.to_datetime(daily_crashes.crash_date)
 
-daily_crashes.crash_date = pd.to_datetime(daily_crashes.crash_date)
-daily_crashes = daily_crashes.set_index('crash_date')
+	# Only include 2015 - 2019 crash data
+	daily_crashes = daily_crashes[daily_crashes["crash_date"] >= '2015-01-01']
+	daily_crashes = daily_crashes[daily_crashes["crash_date"] <= '2019-12-31']
+	return daily_crashes
 
-daily_crashes['crashes'].plot(linewidth=0.5)
-plt.show()
+# Crashes by Day, pretty noisey
+def plot_crashes_by_day():
+	daily_crashes.plot(x='crash_date', y='crashes',linewidth=0.5)
+	plt.show()
 
+# Injuries by Day, pretty noisey
+def plot_injuries_by_day():
+	daily_crashes.plot(x='crash_date', y='injuries',linewidth=0.5)
+	plt.show()
 
-# print(daily_crashes.dtypes)
+def injuries_by_week_of_year():
+	weekly_crashes = daily_crashes.groupby(by=["crash_year", "crash_woy"]).sum()
+	weekly_crashes.unstack("crash_year").plot(y='injuries')
+	plt.show()
+# weekly_crashes.plot(x='crash_woy', y='crashes',linewidth=0.5)
+# plt.show()
+
+if __name__ == "__main__":
+	daily_crashes = get_crash_data()
+	plot_crashes_by_day()
+	plot_injuries_by_day()
+	injuries_by_week_of_year()
+
+	# daily_crashes['dow'] = daily_crashes['crash_date'].dt.weekday
+	# print(daily_crashes)
+
+	# # print(daily_crashes['crash_date'].dt.weekday)
+	# daily_crashes['crash_week_start'] = daily_crashes['crash_date'] - pd.timedelta(days=daily_crashes['dow'])
+	# print(daily_crashes)
+	# plot_injuries_by_day()
+	# crashes_by_week_of_year()
--- get_nyc_crash_data.py
+++ get_nyc_crash_data.py
@@ -13,21 +13,41 @@ client = Socrata("data.cityofnewyork.us",
                  username=username,
                  password=password)
 
-# Day-level aggregation of NYC crashes, injuries, and deaths
-daily_crashes = client.get("h9gi-nx95",
-						 select="crash_date, \
+# # Day-level aggregation of NYC crashes, injuries, and deaths
+# daily_crashes = client.get("h9gi-nx95",
+# 						 select="crash_date, \
+# 						 	date_extract_y(crash_date) AS crash_year, \
+# 						 	date_extract_m(crash_date) AS crash_month, \
+# 						 	date_extract_woy(crash_date) AS crash_woy, \
+# 						 	COUNT(crash_date) as crashes, \
+# 						 	SUM(number_of_persons_injured) AS injuries, \
+# 						 	SUM(number_of_persons_killed) AS deaths",
+# 						 group="crash_date",
+# 						 # where="crash_date >= 2015-01-01T00:00:00.000",
+# 						 order="crash_date ASC",
+# 						 limit=5000)
+
+# daily_crash_df = pd.DataFrame.from_records(daily_crashes)
+# daily_crash_df.to_csv("nyc_crash_data/daily_crash_data.csv", index=False)
+
+# Week-level aggregation of NYC crashes, injuries, and deaths
+weekly_crashes = client.get("h9gi-nx95",
+						 select=" \
+						 	date_extract_woy(crash_date) AS crash_woy, \
 						 	date_extract_y(crash_date) AS crash_year, \
 						 	date_extract_m(crash_date) AS crash_month, \
-						 	date_extract_woy(crash_date) AS crash_woy, \
+						 	MIN(crash_date) AS crash_week_start, \
 						 	COUNT(crash_date) as crashes, \
 						 	SUM(number_of_persons_injured) AS injuries, \
 						 	SUM(number_of_persons_killed) AS deaths",
-						 group="crash_date",
+						 group="crash_woy, crash_year",
 						 # where="crash_date >= 2015-01-01T00:00:00.000",
 						 order="crash_date ASC",
 						 limit=5000)
 
-daily_crash_df = pd.DataFrame.from_records(daily_crashes)
-daily_crash_df.to_csv("nyc_crash_data/daily_crash_data.csv", index=False)
+print(weekly_crash_df)
+
+weekly_crash_df = pd.DataFrame.from_records(weekly_crashes)
+weekly_crash_df.to_csv("nyc_crash_data/weekly_crash_data.csv", index=False)
 
 client.close()
